2dae67d293a9eb4ecf3346a318375d909b3bebee
Rex1121 <rex4832@gmail.com>
2025-03-28 21:01:25 +0100

    Test Gun

---
+++ Weapon.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 40f;
+    public float bulletPrefabLifeTime = 2f;
+
+    void Update()
+    {
+        // Left Mouse Click
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+        
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+}
